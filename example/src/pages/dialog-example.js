fbc4d324323087117b8bedeee0067693a6e46537
PolGuixe <pol.guixe@gmail.com>
2019-02-17 20:37:04 +0100

    updated example!

--- example/src/pages/dialog-example.js
+++ example/src/pages/dialog-example.js
@@ -5,11 +5,17 @@ import MUICookieConsent from "material-ui-cookie-consent"; // using link
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-
 const IndexPage = () => (
   <Layout>
-    <MUICookieConsent debug componentType="Dialog" message="This site uses cookies and you accept.... bla bla bla... legal text provided by the legal team and so on... "/>
-    <SEO title="Material UI Cookie Consent Component" keywords={[`Material UI`, `react`, `cookies`]} />
+    <MUICookieConsent
+      debug
+      componentType="Dialog"
+      message="This site uses cookies and you accept.... bla bla bla... legal text provided by the legal team and so on... "
+    />
+    <SEO
+      title="Material UI Cookie Consent Component"
+      keywords={[`Material UI`, `react`, `cookies`]}
+    />
     <h1>Example page for Material UI Cookie Consent</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
